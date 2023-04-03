6bec39ace383a9d9ebb0505a2cd313b384183f3a
Otaku <trungnv1@athena.studio>
2023-04-03 13:07:14 +0700

    Add motivational quotes array

---
+++ script.js
@@ -0,0 +1,8 @@
+const motivational_quotes = [
+    "Concentrate all your thoughts upon the work in hand. The sun's rays do not burn until brought to a focus."
+    , "Either you run the day or the day runs you."
+    , "I'm a greater believer in luck, and I find the harder I work the more I have of it."
+    , "When we strive to become better than we are, everything around us becomes better too."
+    , "Opportunity is missed by most people because it is dressed in overalls and looks like work."
+    , "Setting goals is the first step in turning the invisible to the visible."
+    , "It's not about better time management. It's about better life management."];

d4f7f93f223434e20a239401951fe0db9cd4a7f4
Otaku <trungnv1@athena.studio>
2023-04-04 12:35:18 +0700

    Update meals

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-const motivational_quotes = [
-    "Concentrate all your thoughts upon the work in hand. The sun's rays do not burn until brought to a focus."
-    , "Either you run the day or the day runs you."
-    , "I'm a greater believer in luck, and I find the harder I work the more I have of it."
-    , "When we strive to become better than we are, everything around us becomes better too."
-    , "Opportunity is missed by most people because it is dressed in overalls and looks like work."
-    , "Setting goals is the first step in turning the invisible to the visible."
-    , "It's not about better time management. It's about better life management."];
+const breakfast = ['Instant Pot Oatmeal', 'Avocado Toast', 'Scrambled Eggs', 'Yogurt with Fruit or Granola'
+, 'Toad in the Hole', 'Cottage Cheese with Fruit', 'Bananas and Peanut Butter', '2 Ingredient Pancakes', 'Egg McMuffin'
+, 'Monte Cristo Sandwich'];
+const lunch = ['Vegan chickpea curry jacket potatoes', 'Salmon salad with sesame dressing'
+, 'Smoky tomato, chipotle & charred corn soup', 'Herby warm cucumbers with lemon', 'Lemon & spinach rice with feta'
+, 'Healthy pesto eggs on toast', 'Cavolo nero orecchiette', 'Chicken satay salad'
+, 'Veggie olive wraps with mustard vinaigrette', 'Bulgur & aubergine pilaf'];
+const dinner = ['Creamy Chicken Soup', 'Stovetop Burgers', 'Crockpot Chicken Breast Recipe', 'Birria Tacos'
+, 'Chili Mac Recipe', 'Air Fryers Chicken Thighs', 'Baked Ranch Chicken with Bacon', 'Crockpot Taco Soup'
+, 'Homemade Alfredo Sauce', 'Oven Fried Chicken Breast'];
